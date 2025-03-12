566f8d42f1221c16d9672d3f0e28a936517d76c7
devhimelali <dev.himelali@gmail.com>
2025-03-12 22:27:51 +0600

    feat(ui): add admin dashboard with widgets & sidebar navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -25,8 +25,8 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-white shadow dark:bg-gray-800">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -42,4 +42,158 @@
 
         @livewireScripts
     </body>
+</html> --}}
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-sidebar="dark"
+    data-sidebar-size="lg" data-preloader="disable" data-theme="default" data-topbar="light" data-bs-theme="light">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>@yield('title') - {{ env('APP_NAME') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Minimal Admin & Dashboard Template" name="description">
+    <meta content="{{ env('APP_NAME') }}" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+    <!-- Fonts css load -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link id="fontsLink"
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap"
+        rel="stylesheet">
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        <!-- ========== App Menu ========== -->
+        <div class="app-menu navbar-menu">
+            <!-- LOGO -->
+            <div class="navbar-brand-box">
+                <a href="{{ route('dashboard') }}" class="logo logo-dark">
+                    <span class="logo-sm">
+                        <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="22">
+                    </span>
+                </a>
+                <a href="{{ route('dashboard') }}" class="logo logo-light">
+                    <span class="logo-sm">
+                        <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="22">
+                    </span>
+                </a>
+                <button type="button" class="p-0 btn btn-sm fs-3xl header-item float-end btn-vertical-sm-hover"
+                    id="vertical-hover">
+                    <i class="ri-record-circle-line"></i>
+                </button>
+            </div>
+
+            <div id="scrollbar">
+                @include('layouts.partials.sidebar')
+                <!-- Sidebar -->
+            </div>
+
+            <div class="sidebar-background"></div>
+        </div>
+        <!-- Left Sidebar End -->
+        <!-- Vertical Overlay-->
+        <div class="vertical-overlay"></div>
+        <header id="page-topbar">
+            <div class="layout-width">
+                @include('layouts.partials.topbar')
+            </div>
+        </header>
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div><!--end row-->
+            </div>
+            <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+
+        @include('layouts.partials.footer')
+    </div>
+    <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+    <!--start back-to-top-->
+    <button class="btn btn-dark btn-icon" id="back-to-top">
+        <i class="bi bi-caret-up fs-3xl"></i>
+    </button>
+    <!--end back-to-top-->
+
+    <!--preloader-->
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner-border text-primary avatar-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="customizer-setting d-none d-md-block">
+        <div class="p-2 shadow-lg btn btn-info text-uppercase rounded-end-0" data-bs-toggle="offcanvas"
+            data-bs-target="#theme-settings-offcanvas" aria-controls="theme-settings-offcanvas">
+            <i class="mb-1 bi bi-gear"></i> Customizer
+        </div>
+    </div>
+
+    <!-- Theme Settings -->
+    @include('layouts.partials.theme-setting')
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            if (document.querySelector("[toast-list]")) {
+                let script = document.createElement("script");
+                script.src = "{{ asset('assets/libs/toastify-js/src/toastify.js') }}";
+                document.body.appendChild(script);
+            }
+
+            if (document.querySelector("[data-provider]")) {
+                let script = document.createElement("script");
+                script.src = "{{ asset('assets/libs/flatpickr/flatpickr.min.js') }}";
+                document.body.appendChild(script);
+            }
+
+            if (document.querySelector("[data-choices]")) {
+                let script = document.createElement("script");
+                script.src = "{{ asset('assets/libs/choices.js/public/assets/scripts/choices.min.js') }}";
+                document.body.appendChild(script);
+            }
+        });
+    </script>
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+</body>
+
 </html>
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+
+    <div id="two-column-menu">
+    </div>
+    <ul class="navbar-nav" id="navbar-nav">
+
+        <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+        <li class="nav-item">
+            <a href="apps-calendar.html" class="nav-link menu-link"> <i class="ph-gauge"></i> <span
+                    data-key="t-dashboards">Dashboards</span> </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="apps-file-manager.html" class="nav-link menu-link"> <i class="ph-folder-open"></i> <span
+                    data-key="t-file-manager">File Manager</span> </a>
+        </li>
+    </ul>
+</div>
